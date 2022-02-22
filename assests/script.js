d2ca2e1a66ac463167c79654c2fac85b2f2c25ae
sp381 <sarah.powell381@gmail.com>
2022-02-22 11:05:25 -0700

    added JS for the questions and answers

--- assests/script.js
+++ assests/script.js
@@ -1,16 +1,45 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById ('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+const shuffledQuestions, currentQuestionIndex
+
 startButton.addEventListener('click', startGame)
+
 function startGame() {
     console.log('Started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
 
 }
 
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        questions: '1. Which of the following functions is a valid type of function that javascript supports?',
+        answers: [
+            {text:'A. Named function', correct: false},
+            {text: 'B. Anonymous function', correct: false},
+            {text: 'C. Both A and B are true', correct: true},
+            {text: 'D. None of the above', correct: false},
+        ]
+    }
+    
+]
 
 
 
