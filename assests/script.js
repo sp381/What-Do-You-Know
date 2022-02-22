cdefec96343fb6ab0d38ad28df34df1f738f834c
sp381 <sarah.powell381@gmail.com>
2022-02-22 11:26:40 -0700

    anything I might have missed committing last time

--- assests/script.js
+++ assests/script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById ('next-btn')
 const questionContainerElement = document.getElementById ('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
