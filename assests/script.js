e9012ca6063765deae1f035891925dd6351fe654
sp381 <sarah.powell381@gmail.com>
2022-02-22 10:44:33 -0700

    added css style sheet, started on JS functions

--- assests/script.js
+++ assests/script.js
@@ -1,19 +1,35 @@
-var buttonEl = document.querySelector("#quiz");
-var possibleAnswerEl = document.querySelector("#possible-answer");
+const startButton = document.getElementById('start-btn')
+startButton.addEventListener('click', startGame)
+function startGame() {
+    console.log('Started')
+}
 
-buttonEl.addEventListener("click", function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "answer-list";
-    listItemEl.textContent = "multiple choice answer";
-    possibleAnswerEl.appendChild(listItemEl)
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
+
+
+
+//var buttonEl = document.querySelector("#quiz");
+//var possibleAnswerEl = document.querySelector("#possible-answer");
+
+// buttonEl.addEventListener("click", function() {
+//     var listItemEl = document.createElement("li");
+//     listItemEl.className = "answer-list";
+//     listItemEl.textContent = "multiple choice answer";
+//     possibleAnswerEl.appendChild(listItemEl)
 
     
-    var answerInput = document.querySelector("#answer-input");
-    var answerInputclick = document.querySelector("#answer-input");
+//     var answerInput = document.querySelector("#answer-input");
+//     var answerInputclick = document.querySelector("#answer-input");
 
-    answerInputClick.addEventListener("click", insertAnswer);
+//     answerInputClick.addEventListener("click", insertAnswer);
 
-});
+// });
 
 
  
