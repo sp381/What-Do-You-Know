5e235ed2ea6d080ba388f91a70221dfe49c3270b
sp381 <sarah.powell381@gmail.com>
2022-02-25 19:37:20 -0700

    added setInterval and clearInterval

--- assests/script.js
+++ assests/script.js
@@ -1,43 +1,53 @@
 const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById ('next-btn')
+//const nextButton = document.getElementById ('next-btn')
 const questionContainerElement = document.getElementById ('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
-
+const timerEl = document.getElementById('countdown');
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
+    //call on timer function here
     console.log('Started')
-    startButton.classList.add('hide')
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
-    questionContainerElement.classList.remove('hide')
     setNextQuestion()
+    var counter = 60
+var countdown = function() {
+    console.log(counter);
+    counter--;
+    if(counter === 0){
+    } 
+};
 }
 
+//Study set Interval and clear Interval 
 function setNextQuestion() {
     resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+    showQuestion(questions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question
-    question.answer.forEach(answer => {
+    console.log(question.answers)
+    question.answers.forEach (answer => {
         const button = document.createElement('button')
         button.innerText = answer.text
         button.classList.add('btn')
         if (answer.correct) {
             button.dataset.correct = answer.correct
+        } else {
+            console.log('wrong answer');
         }
         button.addEventListener('click',selectAnswer)
         answerButtonsElement.appendChild(button)
     })
+    currentQuestionIndex= currentQuestionIndex+1
 }
 
 function resetState() {
-    nextButton.classList.add('hide')
+   // nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild
         (answerButtonsElement.firstChild)
@@ -45,40 +55,63 @@ function resetState() {
 }
 
 function selectAnswer() {
-
+    if(currentQuestionIndex === questions.length) {
+        alert('End of Quiz')
+        //call on an end quiz function here
+    }
+    console.log('hello world')
+    console.log(currentQuestionIndex)
+    setNextQuestion()
 }
-
+//create end quiz function
+    //clear interval for timer
+    //Hide Elements to display end screen
+//Create save score function 
 const questions = [
     {
         question: '1. Which of the following functions is a valid type of function that javascript supports?',
         answers: [
-            {text:'A. Named function', correct: false},
+            {text:' A. Named function', correct: false},
             {text: 'B. Anonymous function', correct: false},
             {text: 'C. Both A and B are true', correct: true},
             {text: 'D. None of the above', correct: false},
         ]
-    }
+    },
+
+    {
+        question: '2. Which method returns the character at the specified index?',
+        answers: [
+            {text: 'A characterAt()', correct: false},
+            {text: 'B. getCharAt()', correct: false},
+            {text: 'C. charAt()', correct: true},
+            {text: 'D. None of the above', correct: false},
+        ]
+    },
+
+    {
+        question: '3. Which of the following is not a mouse event?',
+        answers: [
+            {text: 'A. onmousescroller', correct: true},
+            {text: 'B. onclick', correct: false},
+            {text: 'C. onmouseover', correct: false},
+            {text: 'D. onmousemove', correct: false},
+        ]
+    },
+
+    {
+        question: '4. The opposite of onmouseover is_____?',
+        answers: [
+            {text: 'A. onmouseoff', correct: false},
+            {text: 'B. onmouseout', correct: true},
+            {text: 'C. onmouseunder', correct: false},
+            {text: 'D. onnotmouseover', correct: false},
+        ]
+    },
     
 ]
 
-
-
-//var buttonEl = document.querySelector("#quiz");
-//var possibleAnswerEl = document.querySelector("#possible-answer");
-
-// buttonEl.addEventListener("click", function() {
-//     var listItemEl = document.createElement("li");
-//     listItemEl.className = "answer-list";
-//     listItemEl.textContent = "multiple choice answer";
-//     possibleAnswerEl.appendChild(listItemEl)
-
-    
-//     var answerInput = document.querySelector("#answer-input");
-//     var answerInputclick = document.querySelector("#answer-input");
-
-//     answerInputClick.addEventListener("click", insertAnswer);
-
-// });
-
+var saveScore = function() {
+    localStorage.setItem("score",score);
+}
 
  
